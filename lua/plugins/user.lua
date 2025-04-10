07a0d9961d592d3ba831a2b07f40aa124acf2040
= <sauvehoover@gmail.com>
2025-04-10 13:09:26 -0700

    feat: migration to v5

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- You can also add or configure plugins by creating files in this `plugins/` folder
 -- PLEASE REMOVE THE EXAMPLES YOU HAVE NO INTEREST IN BEFORE ENABLING THIS FILE
 -- Here are some examples:
@@ -85,4 +83,5 @@ return {
       )
     end,
   },
+  { "cordx56/rustowl", dependencies = { "neovim/nvim-lspconfig" } },
 }
